8563a56e8bec92577ca067e228eb10b1c6ea3d0a
João Pedro <joaop@simbioseventures.com>
2021-05-18 10:47:36 -0300

    Change function exception handler for yml generation

--- sabot/kernel/src/main/java/com/dremio/exec/expr/fn/FunctionImplementationRegistry.java
+++ sabot/kernel/src/main/java/com/dremio/exec/expr/fn/FunctionImplementationRegistry.java
@@ -267,7 +267,7 @@ public class FunctionImplementationRegistry implements FunctionLookupContext {
               put("parameterType", parameter.getType().toString());
               try {
                 put("parameterFormat", (parameter.getType().toMinorType()));
-              } catch (Exception exception) {}
+              } catch (Exception ignored) {}
             }
           }
         );
