4cf3f37f36e56e0ea698b95caa942bb41bf87e55
Jose Almeida <almeidajcr90@gmail.com>
2021-08-25 16:14:46 -0300

    Remove extra empty line

--- services/arrow-flight/src/test/java/com/dremio/service/flight/AbstractTestFlightSqlServer.java
+++ services/arrow-flight/src/test/java/com/dremio/service/flight/AbstractTestFlightSqlServer.java
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.dremio.service.flight;
 
 import static java.util.Arrays.asList;
